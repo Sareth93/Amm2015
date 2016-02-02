89e1f39d1936421fb5c415b2c6b7dff9a9405b92
Sareth93 <sareth0193@gmail.com>
2016-02-02 18:36:04 +0100

    iniziato main e creato file javascript per l'ora

---
+++ main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equip="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        <script  type="text/javascript" src="time.js"></script>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <div id="page">
+            <div id="header"></div>
+            <div id="time"></div><script type="text/javascript">window.onload=time();</script>
+        </div>
+    </body>
+</html>
---
+++ time.js
@@ -0,0 +1,18 @@
+function time(){
+    var date=new Date();
+    var hour=date.getHours();
+    var min=date.getMinutes();
+    var sec=date.getSeconds();
+    
+    if(hour<10)
+        hour="0"+hour;
+    if(min<10)
+        min="0"+min;
+    if(sec<10)
+        sec="0"+sec;
+    
+    var t=hour+":"+min+":"+sec;
+    document.getElementById("time").innerHTML=t;
+    setTimeout('time();','1000');
+}
+
