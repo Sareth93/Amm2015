b20af12a7051b8e778b69dff95149a703b09280a
Sareth93 <sareth0193@gmail.com>
2016-02-02 19:44:37 +0100

    aggiunti controlli login-logout

--- Viewer/loginDone.php
+++ Viewer/loginDone.php
@@ -1,5 +1,10 @@
 <?php
-    $user=$_SESSION
+    $user=$_SESSION['username'];
+    
+    echo "Accesso effettuato. Benvenuto $user!";
+    
+    echo "<form action='index.php?arg=logout' meth='Post'><button type='logout' name='logout'>Logout</button></form>";
+    
 ?>
 
 
