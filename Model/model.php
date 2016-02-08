ea182ee9bf35d636952dbd729692ae70b6268836
Sareth93 <sareth0193@gmail.com>
2016-02-08 19:37:10 +0100

    change

--- Model/model.php
+++ Model/model.php
@@ -1,27 +1,27 @@
 <?php
     
     class model{
-        private static $mysql;
+        private static $mysqli;
         private static $usrRoot="accaFederico";
         private static $pwdRoot="picchio8034";
         private static $db="amm15_accaFederico";
         
         public function _construct(){
-            self::$mysql=new mysqli();
+            self::$mysqli=new mysqli();
             session_start();
         }
         
         private function connectToDB(){
-            self::$mysql->connect("localhost",self::$usrRoot, self::$pwdRoot, self::$db);
+            self::$mysqli->connect("localhost",self::$usrRoot, self::$pwdRoot, self::$db);
         }
         
         //gestione login
         public function login(){
             if(isset($_REQUEST['username'])&& isset($_REQUEST['password'])){
                 $this->connectToDB();
-                if(self::$mysql->errno>0)
+                if(self::$mysqli->errno>0)
                     return "Error";
-                $result=self::$mysql->query("SELECT username, password, id FROM users;");
+                $result=self::$mysqli->query("SELECT username, password, id FROM users;");
                 while($row=$result->fetch_row()){
                     if(($_REQUEST['username']==$row[0])&& ($_REQUEST['password']==$row[1])){
                         $_SESSION["loggedIn"]=true;
@@ -51,11 +51,11 @@
         //elenco Canzoni
         public function songs(){
             $this->connectToDB();
-            if(self::$mysql->errno>0)
+            if(self::$mysqli->errno>0)
                 return "Login Error";
             
-            $result= self::$mysql->query("SELECT title, artist, songs.id FROM songs, artists WHERE artists.id=songs.artist_id;");
-            if(self::$mysql->errno>0)
+            $result= self::$mysqli->query("SELECT title, artist, songs.id FROM songs, artists WHERE artists.id=songs.artist_id;");
+            if(self::$mysqli->errno>0)
                 return "Error";
             else
                 return $result;
@@ -64,10 +64,10 @@
         //elenco Artisti
         public function artists(){
             $this->connectToDB();
-            if(self::$mysql->errno>0)
+            if(self::$mysqli->errno>0)
                 return "Login Error";
             $result= self::$mysql->query("SELECT name, id FROM artists;");
-            if(self::$mysql->errno>0)
+            if(self::$mysqli->errno>0)
                 return "Error";
             else
                 return "Ok";
