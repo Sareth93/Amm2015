dcc25c33f9c364b122088fd725a6a1a3976b4b73
Sareth93 <sareth0193@gmail.com>
2016-02-12 19:11:53 +0100

    modifiche finali

--- Viewer/loginSucc.php
+++ Viewer/loginSucc.php
@@ -1,13 +1,8 @@
 <?php
     $user=$_SESSION['username'];
     
-    echo "Accesso effettuato. Benvenuto $user!";
-    
-    echo "<form action='index.php?arg=logout' meth='Post'><button type='logout' name='logout'>Logout</button></form>";
-    
-    echo"<form action='index.php?arg=logout' method='POST'>
-         <button type='Logout' name='logout'>Logout</button>
-         </form>";
+    echo "Accesso effettuato. Benvenuto $user!";    
+    echo "<form action='index.php?arg=logout' meth='POST'><button type='logout' name='logout'>Logout</button></form>";
 ?>
 
 
--- Viewer/loginTry.php
+++ Viewer/loginTry.php
@@ -5,10 +5,10 @@
         <meta http-equip="Content-Type" content="text/html; charset=utf-8">
     </head>
     <body>
-        <h3>Per poter navigare sul sito è necessario effettuare il login</h3>
+        <h3>Per poter navigare sul sito &egrave; necessario effettuare il login</h3>
         <form action="index.php?arg=login" method="POST">
             <label>Username</label>
-            <input id="username" value="" name="username" type="text" required="required" />
+            <input id="username" value="" name="username" type="text" required="required"/>
             <br>
             <label>Password</label>
             <input id="password" name="password" type="password" required="required"/>
