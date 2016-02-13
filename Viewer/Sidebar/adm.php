8b0ef51ba7db1b097db949268682e38243f55dfa
Sareth93 <sareth0193@gmail.com>
2016-02-13 19:39:15 +0100

    modifiche

--- Viewer/Sidebar/adm.php
+++ Viewer/Sidebar/adm.php
@@ -2,8 +2,7 @@
     $user=$_SESSION['username'];
     echo '<div id=tab><a href="index.php?arg=logout">';
     echo $user;
-    echo "/logout";
-    echo "</a></div>";
+    echo "/logout </a></div>";
     echo "<div id='tab'><a href='index.php?arg=songs'>Lista Canzoni</a></div>";
     echo "<div id='tab'><a href='index.php?arg=addArtist'>Aggiungi un autore</a></div>";
 ?>
--- Viewer/Sidebar/usr.php
+++ Viewer/Sidebar/usr.php
@@ -1,7 +1,9 @@
 <?php
     $user=$_SESSION['username'];
-    echo '<div id="tab"><a href="index.php?arg=logout">';
+    echo "<div id='tab'><a href='index.php?arg=logout'>";
     echo $user;
-    echo "/ logout";
-    echo '</a></div>';
+    echo "/logout </a></div>";
+    echo "<div id='tab'><a href='index.php?arg=songs'>Lista canzoni</a></div>";
+    echo "<div id='tab'><a href='index.php?arg=favoriteList'>Lista preferiti</a></div>";
+    echo "<div id='tab'><a href'index.php?arg=addFavorite'>Aggiungi un preferito</a></div>";
 ?>
