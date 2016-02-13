026992e6ea8ff0b5115ed0058b423a0d2b068182
Sareth93 <sareth0193@gmail.com>
2016-02-13 19:40:28 +0100

    modifiche

--- Viewer/Sidebar/usr.php
+++ Viewer/Sidebar/usr.php
@@ -5,5 +5,5 @@
     echo "/logout </a></div>";
     echo "<div id='tab'><a href='index.php?arg=songs'>Lista canzoni</a></div>";
     echo "<div id='tab'><a href='index.php?arg=favoriteList'>Lista preferiti</a></div>";
-    echo "<div id='tab'><a href'index.php?arg=addFavorite'>Aggiungi un preferito</a></div>";
+    echo "<div id='tab'><a href='index.php?arg=addFavorite'>Aggiungi un preferito</a></div>";
 ?>
