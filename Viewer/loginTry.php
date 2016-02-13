c24cad7839e3d438267bf3aa1877125bb34ccad2
Sareth93 <sareth0193@gmail.com>
2016-02-13 17:24:31 +0100

    modifiche

--- Viewer/loginTry.php
+++ Viewer/loginTry.php
@@ -8,10 +8,10 @@
         <h3>Per poter navigare sul sito &egrave; necessario effettuare il login</h3>
         <form action="index.php?arg=login" method="POST">
             <label>Username</label>
-            <input id="username" value="" name="username" type="text" required="required"/>
+            <input id="usr" value="" name="usr" type="text" required="required"/>
             <br>
             <label>Password</label>
-            <input id="password" name="password" type="password" required="required"/>
+            <input id="pwd" name="pwd" type="password" required="required"/>
             <br>
             <button type="login" name="login">Login</button>            
         </form>
