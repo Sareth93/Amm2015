fa7eb1104feec032d576a9df9aeddc2898766a8c
Sareth93 <sareth0193@gmail.com>
2016-02-13 20:37:57 +0100

    modifiche

--- Model/model.php
+++ Model/model.php
@@ -91,11 +91,11 @@
             $this->connectToDB();           
             if(self::$mysqli->errno>0)
                 return "Login Error";         
-            $result= self::$mysqli->query("SELECT title, song_id, artistName FROM songs, artists WHERE artists.artist_id=songs.artist_id;");
-            if(self::$mysqli->errno>0){
+            $result= self::$mysqli->query("SELECT title, songs.song_id, artistName FROM songs, artists WHERE artists.artist_id=songs.artist_id;");
+            /*if(self::$mysqli->errno>0){
                 echo 'ciao1';
                 return "Error";}
-            else
+            else*/
                 return $result;
         }
         //aggiunta canzone
