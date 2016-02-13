0fed8ae0a50b45e004474e2fffc1d4d33b339764
Sareth93 <sareth0193@gmail.com>
2016-02-13 18:24:29 +0100

    modifiche

--- Model/model.php
+++ Model/model.php
@@ -36,11 +36,16 @@
                 return "Error";            
         }        
         //gestione logout e terminazione sessione
-        public function logout(){
+        public function logout(){            
             $_SESSION=array();
+            if(isset($_SESSION[session_name()])){
+                setcookie(session_name(),'',time()-3600);
+            }
+            session_destroy();
+            /*$_SESSION=array();
             if(session_id()!="" || isset($_COOKIE[session_name()]))
                 setcookie(session_name (),'',time()-2592000,'/');
-            session_destroy();                    
+            session_destroy();     */               
         }
         //inserimento nuova utente
         public function newUser(){
