439bb59f04fdb3f9fae55892674d1daabddee551
Sareth93 <sareth0193@gmail.com>
2016-02-13 19:19:17 +0100

    modifiche

--- Viewer/Sidebar/adm.php
+++ Viewer/Sidebar/adm.php
@@ -2,7 +2,9 @@
     $user=$_SESSION['username'];
     echo '<div id=tab><a href="index.php?arg=logout">';
     echo $user;
-    echo "/ logout";
+    echo "/logout";
     echo "</a></div>";
+    echo "<div id='tab'><a href='index.php?arg=songs'>Lista Canzoni</a></div>";
+    echo "<div id='tab'><a href='index.php?arg=addArtist'>Aggiungi un autore</a></div>";
 ?>
 
