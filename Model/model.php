41f9f6b09625adc0c41489a3a2f00367829b47ab
Sareth93 <sareth0193@gmail.com>
2016-02-14 19:28:03 +0100

    modifiche

--- Model/model.php
+++ Model/model.php
@@ -126,7 +126,7 @@
                 echo"ciaofavorites1";
                 return "Error";}
             $result=self::$mysqli->query("SELECT title, artistName, users.username FROM songs, artists, users
-                                          WHERE artists.artist_id=songs.artist_id");
+                                          WHERE artists.artist_id=songs.artist_id AND id=favoriteBy");
             if(self::$mysqli->errno>0){
                 echo"ciaofavorites2";
                 return "Error";}
