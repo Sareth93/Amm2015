2ba3e1041c90851e90479eec0dd474ea6fe36bdf
Sareth93 <sareth0193@gmail.com>
2016-02-14 17:18:07 +0100

    modifiche

--- Model/model.php
+++ Model/model.php
@@ -80,7 +80,7 @@
             $this->connectToDB();           
             if(self::$mysqli->errno>0)
                 return "Login Error"; 
-            $result= self::$mysqli->query("SELECT artistName, artist_id FROM artists");
+            $result= self::$mysqli->query("SELECT title, song_id, artistName FROM songs, artists WHERE songs.artist_id=artists.artist_id");
             if(self::$mysqli->errno>0){
                 echo 'ciao1';
                 return "Error";}
--- Viewer/songListAdmin.php
+++ Viewer/songListAdmin.php
@@ -10,11 +10,11 @@
             echo "<form action='index.php?arg=deleteSong' method='POST'>";
             echo "Elenco canzoni";
             echo "<table align='center'";
-            echo "<tr><th>id</th><th>Titolo</th></tr>";
+            echo "<tr><th>Titolo</th><th>Autore</th></tr>";
             while($row=$songs->fetch_row())
-                    echo"<tr><td>$row[1]</td><td>$row[0]</td>";
-                         /*<td><button type='submit' id='delete$row[2]' name='delete' value='$row[2]'>Cancella</button></tr>";                           
-            */echo "</table><br>";
+                    echo"<tr><td>$row[0]</td><td>$row[1]</td>
+                         <td><button type='submit' id='delete$row[2]' name='delete' value='$row[2]'>Cancella</button></tr>";                           
+            echo "</table><br>";
             echo "</form>";
             
             echo "<p>Inserisci nuova canzone</p>";
