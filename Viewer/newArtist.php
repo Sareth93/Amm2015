529012c0d95dd67e07fc59926216ab20dbdbe183
Sareth93 <sareth0193@gmail.com>
2016-02-14 21:17:50 +0100

    modifiche

--- Viewer/newArtist.php
+++ Viewer/newArtist.php
@@ -1,7 +1,7 @@
 <?php
     echo "<p>Inserimento nuovo artista</p>";
     echo "<fomr id='form' action='index.php?arg=addArtist2' method='POST'>
-          <label>Nome Artista</label><input type='text'id='artistName' name='artistName' required='required'/>
+          <label>Nome Artista </label><input type='text'id='artistName' name='artistName' required='required'/>
           <br><button type='submit' id='confirm' name='confirm'>Conferma</button>
           </form>";
 ?> 
