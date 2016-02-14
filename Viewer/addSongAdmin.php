998fe708abd75ae596a673e4511372a748020c91
Sareth93 <sareth0193@gmail.com>
2016-02-14 17:50:28 +0100

    modifiche

--- Viewer/addSongAdmin.php
+++ Viewer/addSongAdmin.php
@@ -1,4 +1,12 @@
 <?php
-              
-
+    echo "Inserisci una nuova canzone";
+    echo "<br><label>Titolo canzone</label>
+          <input type='text' name='title' id='title' required=required'/></br>
+          <label>Artista</label>";
+    while($row=$artist->fetch_row())
+        echo "<option value='$row[1]'>$row[0]</option>";
+    echo "</select><input"
+    
 ?>
+
+                        
