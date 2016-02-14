e231b613401cb4e3400b4ae95625a883c3aacc6f
Sareth93 <sareth0193@gmail.com>
2016-02-14 17:27:19 +0100

    modifiche

--- Model/model.php
+++ Model/model.php
@@ -80,7 +80,7 @@
             $this->connectToDB();           
             if(self::$mysqli->errno>0)
                 return "Login Error"; 
-            $result= self::$mysqli->query("SELECT title, song_id, artistName FROM songs, artists WHERE songs.artist_id=artists.artist_id");
+            $result= self::$mysqli->query("SELECT title, artistName FROM songs, artists WHERE songs.artist_id=artists.artist_id");
             if(self::$mysqli->errno>0)
                 return "Error";
             else
--- Viewer/songListAdmin.php
+++ Viewer/songListAdmin.php
@@ -12,7 +12,7 @@
             echo "<table align='center'";
             echo "<tr><th>Titolo</th><th>Autore</th></tr>";
             while($row=$songs->fetch_row())
-                    echo"<tr><td>$row[0]</td><td>$row[1]</td>
+                    echo"<tr><td>$row[0]</td><td>$row[2]</td>
                          <td><button type='submit' id='delete$row[2]' name='delete' value='$row[2]'>Cancella</button></tr>";                           
             echo "</table><br>";
             echo "</form>";
@@ -24,7 +24,7 @@
                   <label>Artista</label>
                   <select name='artist'>";
                   while($row=$artist->fetch_row())
-                      echo "<option value='$row[2]'>$row[0]</option>";
+                      echo "<option value='$row[1]'>$row[0]</option>";
             echo "</select><input type='submit' id='confirm' name='confirm' value='Add song'/><br>";
         ?>
     </body>
