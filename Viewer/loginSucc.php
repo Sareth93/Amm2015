f3811990c1dd7d2041fb5843bb89106b96807ba1
Sareth93 <sareth0193@gmail.com>
2016-02-14 22:47:04 +0100

    modifiche

--- Viewer/loginSucc.php
+++ Viewer/loginSucc.php
@@ -1,7 +1,7 @@
 <?php
     $user=$_SESSION['username'];    
-    echo "Accesso effettuato. Benvenuto $user!";    
-    //echo "<form action='index.php?arg=logout' meth='POST'><button type='logout' name='logout'>Logout</button></form>";
+    echo "Accesso effettuato. Benvenuto $user!";  
+    echo "<h2>Ora puoi visitare il sito</h2>";
 ?>
 
 
