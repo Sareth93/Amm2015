99b872fb45ece2c0a16870ff32f9ea92f98cfc01
Sareth93 <sareth0193@gmail.com>
2016-02-14 20:53:30 +0100

    modifiche

--- Viewer/favoriteList.php
+++ Viewer/favoriteList.php
@@ -5,17 +5,18 @@
     $songs=array();
     while($row=$result->fetch_row()){
         $songs[]=$row;
-        echo "<tr><td>$row[0]</td><td>$row[1]</td></tr>";
+        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
     }
     echo "</table>";
-    if(count($songs)>0){
+    /*if(count($songs)>0){
         echo "<br>Seleziona la canzone che desideri rimuovere dai preferiti<br>";
         echo "<form action='index.php?arg=removeFavorite' method='POST'>";
         echo "<select name='song'>";
-        while($row=$result->fetch_row())
-            echo"<option value='$row[3]'>$row[0]</option>";
+        foreach($songs as $item){
+            echo "poba";
+            echo"<option value='$item[3]'>$item[0]</option>";}
         echo "</select><br>";
         echo "<input type='submit' id='confirm' name='confirm' value='rimuovi preferito'/>";
         echo "</form>";
-    }
+    }*/
 ?>
