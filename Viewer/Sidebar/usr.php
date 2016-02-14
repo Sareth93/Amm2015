d1a1818238f2095a1895924a35b8fd0d5f30e831
Sareth93 <sareth0193@gmail.com>
2016-02-14 22:58:33 +0100

    modifiche

--- Viewer/Sidebar/usr.php
+++ Viewer/Sidebar/usr.php
@@ -4,4 +4,5 @@
     echo $user;
     echo "/logout </a></div>";
     echo "<div id='tab'><a href='index.php?arg=songs'>Lista canzoni</a></div>";
+    echo "<div id='tab'><a href='index.php?arg=register'>Registrati</a></div>";
 ?>
