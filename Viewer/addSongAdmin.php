7c746947278b240bc7c2e40ce38a5adfe427fc9f
Sareth93 <sareth0193@gmail.com>
2016-02-14 18:06:43 +0100

    modifiche

--- Viewer/addSongAdmin.php
+++ Viewer/addSongAdmin.php
@@ -4,6 +4,6 @@
           <input type='text' name='title' id='title' required=required'/></br>
           <label>Artista</label><select name='artistName'>";
     while($row=$artists->fetch_row())
-        echo "<option value='$row[1]'>$row[1]</option>";
+        echo "<option value='$row[1]'>$row[0]</option>";
     echo "</select><input type='submit' id='confirm' name='confirm' value='Add song'/><br>";    
 ?>
