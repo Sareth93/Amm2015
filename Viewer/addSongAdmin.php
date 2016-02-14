5a8e755d6984cdfd602fee418fe09f990d621399
Sareth93 <sareth0193@gmail.com>
2016-02-14 18:16:37 +0100

    modifiche

--- Viewer/addSongAdmin.php
+++ Viewer/addSongAdmin.php
@@ -2,7 +2,7 @@
     echo "Inserisci una nuova canzone";
     echo "<form action='index.php?arg=addSong' method='POST'>
           <br><label>Titolo canzone</label>
-          <input type='text' name='title' id='title' required=required'/></br>
+          <input type='text' name='title' id='title' required=required'/></br>";
           //<label>Artista</label><select name='artistName'>";
     echo "<tr><th>Artista</th></tr>";
     while($row=$artists->fetch_row())
