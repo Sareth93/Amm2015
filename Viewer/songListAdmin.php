8a80ceddbb8ae9d32198d4047efe8c556f6d26ef
Sareth93 <sareth0193@gmail.com>
2016-02-14 16:36:20 +0100

    modifiche

--- Viewer/songListAdmin.php
+++ Viewer/songListAdmin.php
@@ -12,9 +12,9 @@
             echo "<table align='center'";
             echo "<tr><th>Artista</th>";/*<th>id</th></tr>";*/
             while($row=$songs->fetch_row())
-                    echo"<tr><td>$row[0]</td>
+                    echo"<tr><td>$row[0]</td>";/*<td>$row[1]$row[2]</td> 
                          <td><button type='submit' id='delete$row[1]' name='delete' value='$row[1]'>Cancella</button></tr>";                           
-            echo "</table><br>";
+            */echo "</table><br>";
             echo "</form>";
             
             echo "<p>Inserisci nuova canzone</p>";
