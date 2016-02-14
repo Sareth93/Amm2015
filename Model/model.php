bcafcebdff8d4135f0009407fdad3fff48a693ab
Sareth93 <sareth0193@gmail.com>
2016-02-14 19:23:45 +0100

    modifiche

--- Model/model.php
+++ Model/model.php
@@ -125,7 +125,7 @@
             if(self::$mysqli->errno>0){
                 echo"ciaofavorites1";
                 return "Error";}
-            $result=self::$mysqli->query("SELECT title, artistName, username FROM songs, artist, users
+            $result=self::$mysqli->query("SELECT title, artistName, username FROM songs, artists, users
                                           WHERE artist.artist_id=songs.artist_id AND id=favoriteBy");
             if(self::$mysqli->errno>0){
                 echo"ciaofavorites2";
