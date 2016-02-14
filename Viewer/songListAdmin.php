0a56a4a410b7b846de3f4a4ba996c38973675d98
Sareth93 <sareth0193@gmail.com>
2016-02-14 16:34:23 +0100

    modifiche

--- Viewer/songListAdmin.php
+++ Viewer/songListAdmin.php
@@ -10,10 +10,10 @@
             echo "<form action='index.php?arg=deleteSong' method='POST'>";
             echo "Elenco canzoni";
             echo "<table align='center'";
-            echo "<tr><th>Titolo</th><th>Artista</th></tr>";
+            echo "<tr><th>Artista</th>";/*<th>id</th></tr>";*/
             while($row=$songs->fetch_row())
-                    echo"<tr><td>$row[0]</td><td>$row[1]$row[2]</td> 
-                         <td><button type='submit' id='delete$row[3]' name='delete' value='$row[3]'>Cancella</button></tr>";                           
+                    echo"<tr><td>$row[0]</td>"/*<td>$row[1]$row[2]</td> */
+                         <td><button type='submit' id='delete$row[1]' name='delete' value='$row[1]'>Cancella</button></tr>";                           
             echo "</table><br>";
             echo "</form>";
             
