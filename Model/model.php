493b98cf6b758294fc7a8f633748fec0ee4493f7
Sareth93 <sareth0193@gmail.com>
2016-02-18 18:55:25 +0100

    ultimato

--- Model/model.php
+++ Model/model.php
@@ -68,7 +68,7 @@
                 else{
                     $song_id=$_REQUEST['delete'];
                     $del=self::$mysqli->query("DELETE FROM songs WHERE song_id='$song_id' AND favoriteBy IS NULL");
-                    if(self::$mysqli->errno>0 || self::$mysqli->affected_rows!=1){
+                    if(self::$mysqli->errno>0){
                         echo "deleteSong2";
                         return "Error";}
                     else
